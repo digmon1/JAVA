45d97889821d1143998e00d1d1c9ba09096aa1bb
sanjay kumar shrestha <xboxfamil639@gmail.com>
2026-09-17 09:36:46 +0545

    about<< and >>

--- Operator/BitwiseOP.java
+++ Operator/BitwiseOP.java
@@ -6,8 +6,8 @@ class BitwiseOP{
 		System.out.println(5^6);
 		System.out.println(10<<3);
 		System.out.println(-10<<2);
-		
-		
+		System.out.println(52>>2);
+		System.out.println(-48>>2);
 	}
 
 }
